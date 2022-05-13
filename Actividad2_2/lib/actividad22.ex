cdea5c60fccf0264264660b195f32cfc7af5bd2e
Andrea Yela <73191386+AnYelg@users.noreply.github.com>
2022-05-12 20:53:31 -0500

    Final Actividad2_2

--- Actividad2_2/lib/actividad22.ex
+++ Actividad2_2/lib/actividad22.ex
@@ -1,18 +1,130 @@
-defmodule Actividad22 do
+#Joshua Ruben Amaya Camilo A01025258
+#Andrea Yela González A01025250
+#13 de mayo 2020
+
+defmodule Actividad2_2.Actividad22 do
   @moduledoc """
-  Documentation for `Actividad22`.
+  Funtions to wotk with lists in Elixir
   """
+
+  # Exercise 1
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Actividad22.hello()
-      :world
-
+  Function that receives two elements, a number "n" and a list "lst" with ascendant numbers.
+  Return a new lst with the inserted element "n" in it's correct place
   """
-  def hello do
-    :world
+
+  # Define the function
+  def insert(list, n), do: do_insert(list, n, [])
+
+  # Define base case
+  defp do_insert([], n, result) do    # When receives an empty list
+
+    if n in result do   # The element is in the list
+      Enum.reverse(result)    # Return the reversed value
+
+    else    # The element is not in the list
+      Enum.reverse([n | result])    # Add n and return the list reversed
+
+    end
+
   end
+
+  defp do_insert([hd | tl], n, result) do     # The list contains values
+
+    if hd < n do   # If head is less than n value
+      do_insert(tl, n, [hd | result])
+
+    else    # If head is greater than n value
+
+      if n in result do   # The list contains n value
+        do_insert(tl, n, [hd | result])   # Compare the next value
+
+      else    # The list doesn't contain n value
+        do_insert(tl, n, [hd | [n | result]])   # Append n value
+
+      end
+
+    end
+
+  end
+
+
+
+  # Exercise 2
+
+  @doc """
+  The function insertion_sort receives a list
+  The function sorts a list from smallest to largest value
+  """
+
+  # Define function
+  def insertion_sort(list), do: do_insertion_sort(list, [])
+
+  # Define base case
+  defp do_insertion_sort([], compile), do: compile    # If the list is empty, return the list
+  defp do_insertion_sort([hd | tl], compile) do    # The list contains values
+
+    do_insertion_sort(tl, insert(compile, hd))    # Calls insert function and calls recursively
+
+  end
+
+
+
+  # Exercise 3
+
+  @doc """
+  The function rotate_left receives a list and n value
+  The function rotates list left by n value
+  """
+
+  # Define the function
+  def rotate_left([], n), do: [] # The list is empty, nor defp because it can identify an empty list
+  def rotate_left(list, n) do    # Reverse the list received to generalize the entrant values
+      if n < 0 do   # n value is negative
+          do_rotate_left(Enum.reverse(list), n * -1, true)   # Reverse the list and make positive the n value
+      else    # n value is positive
+          do_rotate_left(list, n, false)   # Keep the list intact
+      end
+  end
+
+  # Define base case
+  defp do_rotate_left(list, 0, result) do    # Reverse or keep the list when n value reaches zero
+    if result do    # The list have been reversed before
+      Enum.reverse(list)   # Reverse the list
+    else
+      list   # Keep intact the list
+    end
+  end
+
+  defp do_rotate_left([hd | tl], n, result) do    # The list contains values
+    do_rotate_left(tl ++ [hd], n - 1, result)   # Append the head into tail n times and decrease n in 1
+  end
+
+
+
+  # Exercise 10
+
+  @doc """
+  Encode function :)
+  """
+  def encode([]), do: [] #If there is no value return nothing
+  def encode(list), do: do_encode(list, nil, 0, []) #If the list is empty return list
+
+  defp do_encode([], hdp, n, return), do: Enum.reverse([{n, hdp} | return])
+  defp do_encode([hd | tl], hdp, n, return) do
+    if hd == hdp do #if the previus value is the same as head
+      do_encode(tl, hdp, n + 1, return) #you move to the next value and add 1 to counter
+    else
+      if hdp == nil do #if its the first value
+        do_encode(tl, hd, 1, return) #look for the tail and begin the process
+      else
+        do_encode(tl, hd, 1, [{n, hdp} | return]) #when the list only has 1 value
+      end
+    end
+  end
+
+
+
 end
+
--- Actividad2_2/test/actividad22_test.exs
+++ Actividad2_2/test/actividad22_test.exs
@@ -1,8 +1,49 @@
-defmodule Actividad22Test do
-  use ExUnit.Case
-  doctest Actividad22
+#Joshua Ruben Amaya Camilo A01025258
+#Andrea Yela González A01025250
+#13 de mayo 2020
 
-  test "greets the world" do
-    assert Actividad22.hello() == :world
+defmodule Actividad2_2.Actividad22Test do
+  use ExUnit.Case
+  alias Actividad2_2.Actividad22
+
+  # Functions
+  #Ejercicio 1
+  test "test insert" do
+    assert Actividad22.insert([], 14) == [14]
+    assert Actividad22.insert([5, 6, 7, 8], 4) == [4, 5, 6, 7, 8]
+    assert Actividad22.insert([1, 3, 6, 7, 9, 16], 5) == [1, 3, 5, 6, 7, 9, 16]
+    assert Actividad22.insert([1, 5, 6], 10) == [1, 5, 6, 10]
   end
+  #Ejercicio 2
+  test "test insertion_sort" do
+    assert Actividad22.insertion_sort([]) == []
+    assert Actividad22.insertion_sort([4, 3, 6, 8, 3, 0, 9, 1, 7]) == [0, 1, 3, 3, 4, 6, 7, 8, 9]
+    assert Actividad22.insertion_sort([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert Actividad22.insertion_sort([5, 5, 5, 1, 5, 5, 5]) == [1, 5, 5, 5, 5, 5, 5]
+  end
+  #Ejercicio 3
+  test "test rotate_left" do
+    assert Actividad22.rotate_left([], 5) == []
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], 0) == [:a, :b, :c, :d, :e, :f, :g]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], 1) == [:b, :c, :d, :e, :f, :g, :a]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], -1) == [:g, :a, :b, :c, :d, :e, :f]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], 3) == [:d, :e, :f, :g, :a, :b, :c]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], -3) == [:e, :f, :g, :a, :b, :c, :d]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], 8) == [:b, :c, :d, :e, :f, :g, :a]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], -8) == [:g, :a, :b, :c, :d, :e, :f]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], 45) == [:d, :e, :f, :g, :a, :b, :c]
+    assert Actividad22.rotate_left([:a, :b, :c, :d, :e, :f, :g], -45) == [:e, :f, :g, :a, :b, :c, :d]
+  end
+  #Ejercicio 10
+  test "test encode" do
+    assert Actividad22.encode([]) == []
+    assert Actividad22.encode([:a]) == [{1, :a}]
+
+    assert Actividad22.encode([:a, :a, :a, :a, :b, :c, :c, :a, :a, :d, :e, :e, :e, :e]) ==
+             [{4, :a}, {1, :b}, {2, :c}, {2, :a}, {1, :d}, {4, :e}]
+
+    assert Actividad22.encode([1, 2, 3, 4, 5]) == [{1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}]
+    assert Actividad22.encode([9, 9, 9, 9, 9]) == [{5, 9}]
+  end
+
 end
