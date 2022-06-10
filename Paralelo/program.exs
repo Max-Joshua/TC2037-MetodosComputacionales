b723651c44c8179611997ead432bdbf478275b63
Andrea Yela <73191386+AnYelg@users.noreply.github.com>
2022-06-10 12:49:34 -0500

    Avance paralelo programa

--- Paralelo/program.exs
+++ Paralelo/program.exs
@@ -20,50 +20,73 @@ defmodule Tlist do
   end
 
 
-  def stateStart([hd | tl]), do:
+  def inicio([hd | tl]), do:
     case hd do
-      "[" -> modoCorchete(tl, "")
-      "{" -> modoLlave(tl, "")
+      "[" -> inicioCorchete(tl, "", "") #Primer elemento es [
+      "{" -> inicioLlave(tl, "", "") #Primer elemento es {
+       _ -> inicio(tl) #No hay nada
   end
 
-  def modoCorchete([hd | tl], build) do
-    modoValor(tl, Enum.join([build, "<span class=\"parentesis\">[</span><br>"]))
+#Build - archivo html (lo que se está esccribiendo)
+#estado - Para saber en que modo estas
+
+#Agrega el corchete al build
+  def inicioCorchete([hd | tl], build, estado) do
+    buscarDato(tl, Enum.join([build, "<span class=\"parentesis\">[</span><br>"]), ["[" | estado])
   end
 
-  def modoCorcheteF([hd | tl], build) do
-    modoValor(tl, Enum.join([build, "<class=\"parentesis\">]</span><br>"]))
+  #Agrega el final del corchete en el build
+  def finalCorchete([_ | tl], build, [_ | estado]) do
+    [sigEstado | _] = estado
+      case sigEstado do
+        "[" -> sigDatoCorchete(tl, Enum.join([build, "<class=\"parentesis\">]</span><br>"]), estado)
+        "{" -> sigDatoLlave(tl, Enum.join([build, "<class=\"parentesis\">]</span><br>"]), estado)
+      end
   end
 
-  def modoIterarCorchete([hd | tl], build) do
+  #Busca que dato sigue despues del Corchete
+  def sigDatoCorchete([hd | tl], build, estado) do
     case hd do
-      "," -> modoCorchete(tl, Enum.join([build, hd, "<br>"]))
-      "]" -> modoCorcheteF(tl, build)
-      _ -> modoIterar(tl, build)
+      #Va a buscar que tipo de dato es
+      "," -> buscarDatoCorchete(tl, Enum.join([build, hd, "<br>"]), estado)
+      #Finaliza el corchete
+      "]" -> finalCorchete([hd | tl], build, estado)
+      #No hay nada (posiblemente hay un espacio)
+      _ -> sigDatoCorchete(tl, build, estado)
+    end
   end
 
-  def modoString([hd | tl], build) do
+  def buscarDatoCorchete([hd | tl], build) do
+    case hd do
+      hd == "[" -> inicioCorchete(tl, build, estado)
+      hd == "{" -> inicioLlave(tl, build, estado)
+      hd == "\""-> datoStringCorchete(tl, Enum.join([build, "<span class=\"string\">\""]), estado)
+      hd == "]" -> finalCorchete(tl, build)
+      Regex.match?(~r/^\d$/, hd) -> datoIntCorchete([hd | tl], Enum.join([build, "<span class=\"number\">"]), estado)
+      Regex.match?(~r/[truefalsn]/, hd) -> datoBoolCorchete([hd | tl], Enum.join([build, "<span class=\"bool\">"]), estado)
+      hd == _ -> buscarDatoCorchete(tl, build)
+    end
+  end
+  def datoIntCorchete() do
+
+  end
+
+  def datoStringCorchete([hd | tl], build) do
     case hd do
       "\""-> modoValor(tl, Enum.join([build, hd, "</span>"]))
       _ -> modoString(tl, Enum.join([build, hd]))
   end
 
-  def modoValor([hd | tl], build) do
-    case hd do
-      "[" -> modoCorchete(tl, build)
-      "{" -> modoLlave(tl, build)
-      "\""-> modoString(tl, Enum.join([build, "<span class=\"string\">\""]))
-      "]" -> modoCorcheteF(tl, build)
-      _ -> modoValor(tl, build)
-  end
 
-  def modoLlave([hd | tl], build) do
+
+  def inicioLlave([hd | tl], build) do
     modoValor(tl, Enum.join([build, "<span class=\"parentesis\">{</span><br>"]))
   end
 
   def modoLlaveF([hd | tl], build) do
     modoValor(tl, Enum.join([build, "<span class=\"parentesis\">}</span><br>"]))
   end
-  
+
   def modoLlaveS([hd | tl], build) do
 
   end
